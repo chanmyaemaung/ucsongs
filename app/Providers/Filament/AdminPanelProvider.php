6be6834b16b99e00ee965afc5884c977ac19030a
Chen Lay <maungmaungyoker.mmy@gmail.com>
2025-03-29 23:29:52 +0700

    feat(branding): add brand logo
    - Include brand logo for the admin panel with specified height
    - Update .gitignore to exclude .DS_Store files
    
    BREAKING CHANGE: None

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,8 @@ class AdminPanelProvider extends PanelProvider
                 'gray' => Color::Slate,
             ])
             ->brandName(__('UC Holy Songs'))
+            ->brandLogo(asset('assets/images/ucsong_logo.svg'))
+            ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
